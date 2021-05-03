26b575449777e1da7f6879b649715bd4b7db6174
autoblack <cclauss@users.noreply.github.com>
2021-05-03 10:21:47 +0000

    fixup: Format Python code with Black

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 def multiply(x, y):
     return x * y
 
-def addition (x,y):
-    return x +y
+
+def addition(x, y):
+    return x + y
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    print ("hi")
-
-
+    print("hi")
--- tests.py
+++ tests.py
@@ -5,6 +5,7 @@ def test_mult():
     res = main.multiply(3, 4)
     assert res == 3 * 4
 
+
 def test_add():
     res = main.addition(3, 4)
     assert res == 3 + 4
