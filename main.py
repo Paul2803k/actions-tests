1e8352beb72937c4bfb339588dc6e173856a2504
Paul2803k <p.scattolin95@gmail.com>
2021-05-03 12:21:20 +0200

    added test

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 def multiply(x, y):
     return x * y
 
+def addition (x,y):
+    return x +y
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    res = multiply(3, 4)
-    assert res == 3 * 4
+    print ("hi")
+
+
--- tests.py
+++ tests.py
@@ -1,7 +1,10 @@
-import pytest
 import main
 
 
 def test_mult():
     res = main.multiply(3, 4)
     assert res == 3 * 4
+
+def test_add():
+    res = main.addition(3, 4)
+    assert res == 3 + 4
